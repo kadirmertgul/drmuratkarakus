5b5c4389dec614b1a65d723d354dd3e5e4833f9c
kadirmertgul <kmert19@hotmail.com>
2024-08-26 15:37:19 +0300

    ui commit

---
+++ nav.php
@@ -0,0 +1,99 @@
+
+<header class="header header-layout1">
+    <div class="header-topbar">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <ul class="contact__list d-flex flex-wrap align-items-center list-unstyled mb-0">
+                            <li>
+                                <button class="miniPopup-emergency-trigger" type="button">24/7 Emergency</button>
+                                <div id="miniPopup-emergency" class="miniPopup miniPopup-emergency text-center">
+                                    <div class="emergency__icon">
+                                        <i class="icon-call3"></i>
+                                    </div>
+                                    <a href="tel:+201061245741" class="phone__number">
+                                        <i class="icon-phone"></i> <span>+2 01061245741</span>
+                                    </a>
+                                    <p>Please feel free to contact our friendly reception staff with any general or medical enquiry.
+                                    </p>
+                                    <a href="appointment.html" class="btn btn__secondary btn__link btn__block">
+                                        <span>Make Appointment</span> <i class="icon-arrow-right"></i>
+                                    </a>
+                                </div><!-- /.miniPopup-emergency -->
+                            </li>
+                            <li>
+                                <i class="icon-phone"></i><a href="tel:+5565454117">Emergency Line: (002) 01061245741</a>
+                            </li>
+                            <li>
+                                <i class="icon-location"></i><a href="#">Location: Brooklyn, New York</a>
+                            </li>
+                            <li>
+                                <i class="icon-clock"></i><a href="contact-us.html">Mon - Fri: 8:00 am - 7:00 pm</a>
+                            </li>
+                        </ul><!-- /.contact__list -->
+                        <div class="d-flex">
+                            <ul class="social-icons list-unstyled mb-0 mr-30">
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            </ul><!-- /.social-icons -->
+                            <form class="header-topbar__search">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <button class="header-topbar__search-btn"><i class="fa fa-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div><!-- /.col-12 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.header-top -->
+    <nav class="navbar navbar-expand-lg sticky-navbar ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">
+                <img src="assets/images/logo/logo-light.png" class="logo-light" alt="logo">
+                <img src="assets/images/logo/logo-dark.png" class="logo-dark" alt="logo">
+            </a>
+            <button class="navbar-toggler" type="button">
+                <span class="menu-lines"><span></span></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mainNavigation">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav__item ">
+                        <a href="index.html has-dropdown"  class="nav__item-link active">Anasayfa</a>
+                    </li><!-- /.nav-item -->
+                    <li class="nav__item has-dropdown">
+                        <a href="about-us.html"  class="nav__item-link">Hakkımızda</a>
+                    </li><!-- /.nav-item -->
+                    <li class="nav__item has-dropdown">
+                        <a href="#" class="dropdown-toggle nav__item-link">Uzmanlıklar</a>
+                        <ul class="dropdown-menu">
+                            <li class="nav__item">
+                                <a href="service1.html.html" class="nav__item-link">uzmanlık1</a>
+                            </li> <!-- /.nav-item -->
+                            <li class="nav__item">
+                                <a href="service2.html.html" class="nav__item-link">uzmanlık2</a>
+                            </li> <!-- /.nav-item -->
+                            <li class="nav__item">
+                                <a href="service3.html.html" class="nav__item-link">uzmanlık3</a>
+                            </li> <!-- /.nav-item -->
+                        </ul><!-- /.dropdown-menu -->
+                    </li><!-- /.nav-item -->
+                    <li class="nav__item">
+                        <a href="contact-us.html" class="nav__item-link">Hasta Videoları</a>
+                    </li><!-- /.nav-item -->
+                    <li class="nav__item">
+                        <a href="contact-us.html" class="nav__item-link">İletişim</a>
+                    </li><!-- /.nav-item -->
+                </ul><!-- /.navbar-nav -->
+                <button class="close-mobile-menu d-block d-lg-none"><i class="fas fa-times"></i></button>
+            </div><!-- /.navbar-collapse -->
+            <div class="d-none d-xl-flex align-items-center position-relative ml-30">
+                <a href="#" class="btn btn__primary btn__rounded ml-30">
+                    <i class="icon-calendar"></i>
+                    <span>Randevu Al</span>
+                </a>
+            </div>
+        </div><!-- /.container -->
+    </nav><!-- /.navabr -->
+</header><!-- /.Header -->
